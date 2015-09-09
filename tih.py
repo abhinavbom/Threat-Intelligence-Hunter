9e2504646ccc43a5bce99a6937ee6ef1cf3fe865
abhinav singh <abhinavbom@gmail.com>
2015-09-09 17:41:53 +0800

    minor fix
    Added list entry to IP arguement.

--- tih.py
+++ tih.py
@@ -14,8 +14,8 @@ print banner
 def main():
     print "Intel test"
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-ip", type=str,  help="Enter the IP address to search through the list of feed")
-
+    parser.add_argument('-ip', type=str, nargs='+', help="Search for a single IP through OSINT threat feeds")
+    parser.add_argument('-list', type=str, nargs='?', help="Search for a list of IP vectors. Accepted formats is .txt")
     args = parser.parse_args()
     if args.ip:
         ip_ioc = parse_ip(args.ip)
@@ -23,4 +23,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
