bb970854421bf3d8bd3881f6142bad9355683595
abhinav singh <abhinavbom@gmail.com>
2015-09-17 10:23:55 +0800

    VT API V2.0 MD5 support
    Adding support for Virustotal API V2.0. This will allow searching of Hashes in VT. 
    The API uses Public API hence a public key would be required.
    Public key restrictions applies(4 requests per minute).

--- bin/md5vt.py
+++ bin/md5vt.py
@@ -1,15 +1,28 @@
-import simplejson
-import urllib
-import urllib2
-from bin.parse import *
+#stdlib imports
+
+import json
+import argparse
+import sys
 import requests
 
-def hashVT():
-    url = "https://www.virustotal.com/vtapi/v2/file/report"
-    parameters = {"resource": "99017f6eebbac24f351415dd410d522d",
-                  "apikey": "100e582a15884a9c5cc37e298766065695e551fb1fc88ee05eadc85eacc3b61e"}
-    data = urllib.urlencode(parameters)
-    req = connect(url)
-    response = urllib2.urlopen(req, headers=create_basic_headers())
-    json = response.read()
-    print json
+#local imports
+
+from bin.parse import *
+
+    
+def getReport(md5):
+    api = ''
+    base = 'https://www.virustotal.com/vtapi/v2/'
+    param = {'resource':md5,'apikey':api}
+    url = base + "file/report"
+    #data = urllib.urlencode(param)
+    #result = urllib2.urlopen(url,data)
+    print "Connecting to Virustotal"
+    r = requests.get(url,
+                    headers=create_basic_headers(),
+                    proxies={'http': HTTP_PROXY, 'https': HTTPS_PROXY},
+                    params=param)
+    print r.json()
+    if r.status_code == 0:
+        print md5 + " -- Not Found in VT"
+        return 0
