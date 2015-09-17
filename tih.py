0ac003e749be0d55a6a88daaf1373105576a1ec4
abhinav singh <abhinavbom@gmail.com>
2015-09-17 15:30:02 +0800

    MD5 support as in issue #7
    Adding argument parsing for MD5 input that is passed to VT api for intel search.

--- tih.py
+++ tih.py
@@ -2,6 +2,7 @@ __author__ = '@abhinavbom a.k.a Darkl0rd'
 
 #local imports
 from bin.parse import *
+from bin.md5vt import *
 
 #stdlib imports
 import argparse
@@ -15,12 +16,16 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-ip', type=str, nargs='+', help="Search for a single IP through OSINT threat feeds")
     parser.add_argument('-list', type=str, nargs='?', help="Search for a list of IP vectors. Accepted formats is .txt")
+    parser.add_argument('-md5', type=str, nargs='?', help="Search for a single of space separated multiple MD5s. This search is performed on Virustotal. Please add your public key to bin/vt.py")
     args = parser.parse_args()
     if args.ip:
-        ip_ioc = parse_ip(args.ip)
-        print ip_ioc
+        parse_ip(args.ip)
+        vt_ip(args.ip)
     if args.list:
         parse_ipList(list)
+    if args.md5:
+        vt_md5(args.md5)
+
 
 if __name__ == '__main__':
     main()
