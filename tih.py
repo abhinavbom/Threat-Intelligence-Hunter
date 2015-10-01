f550afb555c341b0aafa02b6438f5b56afbafd04
abhinav singh <abhinavbom@gmail.com>
2015-10-01 16:11:04 +0800

    adding reputation argparse
    Adding argument to receive domains for reputation lookup.

--- tih.py
+++ tih.py
@@ -1,8 +1,9 @@
 __author__ = '@abhinavbom a.k.a Darkl0rd'
 
 #local imports
-from bin.parse import *
-from lib.vt import *
+from lib.parse import *
+from api.vt import *
+from api.urlvoid import *
 
 #stdlib imports
 import argparse
@@ -15,9 +16,20 @@ def main():
     print "Intel test"
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-ip', type=str, nargs='+', help="Search for a single IP through OSINT threat feeds")
+    
     parser.add_argument('-list', type=str, nargs='?', help="Search for a list of IP vectors. Accepted formats is .txt")
-    parser.add_argument('-md5', type=str, nargs='+', help="Search for a single of space separated multiple MD5s. This search is performed on Virustotal hence only 4 searcges per minute is allowed. Please add your public key to bin/vt.py")
-    parser.add_argument('-url', type=str, nargs='+', help="Search for a single of space separated multiple urls. This search is performed on Virustotal hence only 4 searches per minute is allowed. Please add your public key to bin/vt.py")
+    
+    parser.add_argument('-md5', type=str, nargs='+', help="Search for a single of space separated multiple MD5s. "
+                                                          "This search is performed on Virustotal hence only 4 searches "
+                                                          "per minute is allowed. Please add your public key to bin/vt.py")
+
+    parser.add_argument('-url', type=str, nargs='+', help="Search for a single of space separated multiple urls. "
+                                                          "This search is performed on Virustotal hence only 4 searches "
+                                                          "per minute is allowed. Please add your public key to bin/vt.py")
+
+    parser.add_argument('-repo', type=str, nargs='?', help="Search for the reputation of a list of URLs. The script"
+                                                           "accepts a txt file containing list of domains and searches it"
+                                                           "against popular reputation tools like URLVoid, Bluecoat etc.")
     args = parser.parse_args()
     if args.ip:
         if len(args.ip) > 4:
@@ -37,6 +49,8 @@ def main():
             print "Too many argument values specified. Maximum arguments per minute is 4."
             sys.exit(1)
         vt_url(args.url)
+    if args.repo:
+        urlvoid(args.repo)
 
 if __name__ == '__main__':
     main()
