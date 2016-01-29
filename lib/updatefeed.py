a789a38c8fdf6821bd0495ae1689a69c8ee18d2b
abhinav singh <abhinavbom@gmail.com>
2016-01-29 12:08:17 +0800

    Update updatefeed.py
    adding parse support.

--- lib/updatefeed.py
+++ lib/updatefeed.py
@@ -1,35 +1,33 @@
 __author__ = '@abhinavbom a.k.a darkl0rd'
 
 import urllib2
+import urlparse
 import re
 import os
 import time
 from lib.feeds import *
 from lib.parse import *
 
-if not os.path.exists('intel'):
-    os.mkdir('intel')
-
-
-
-def gather(self):
+def gather():
+    if not os.path.exists('intel'):
+        os.mkdir('intel')
     print "Starting feed update process"
     counter = 0
     ioc_list = []
     timestr = time.strftime("%Y%m%d-%H%M%S")
+    print type(timestr)
     for source in OSINT_IP.iteritems():
-        name = source + timestr
-        file = open(name, 'w')
-        c = connect(source,params=url_param())
-        for line in c:
+        print type(source)
+        print source[0]
+        name = str(source[0]) +"_" + timestr
+        print name
+        print "Starting feed update process-2"
+        file = open(name, 'w+')
+        r = requests.get(str(source[1]),
+                        headers=create_basic_headers(),
+                        proxies={'http': HTTP_PROXY, 'https': HTTPS_PROXY})
+        for line in r:
             if line.startswith("/") or line.startswith('\n') or line.startswith("#"):
                 pass
             else:
                 file.write(line+'\n')
-
-
-
-
-
-
-    
